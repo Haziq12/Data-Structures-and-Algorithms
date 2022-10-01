76cf50021247bd003cff86f2b3b7301a48b4488d
Haziq Naeem <haziqnaeem36@gmail.com>
2022-10-01 16:27:59 -0400

    Time: 191 ms (34.31%), Space: 55.4 MB (37.14%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,49 +3,19 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-//     nums = 1, 2, 3, 4
-//     left = 1, 1, 2, 6
-//     right = 
-//     let solutionArray = []
-//     let left = []
-//     let right = []
-    
-//     for(let i = 0; i < nums.length; i++) {
-//         if(left.length === 0){
-//             left.push(1)
-//         } else {
-//             left.push(nums[i - 1] * left[i - 1])
-//         }
-//     }
-    
-//     for(let i = nums.length-1; i > -1; i--) {
-//         if(right.length === 0) {
-//             right.push(1)
-//         } else {
-//             right.unshift(nums[i + 1] * right[0])
-//         }
-//     }
-    
-//     console.log(left, right)
-    
-//     for(let i = 0; i < left.length; i++) {
-//         solutionArray.push(left[i] * right[i])
-//     }
-    
-//     return solutionArray
-    
-    
-    let left = 1
-    let product = 1
+    let temp = 1
     let result = []
     for(let i = 0; i < nums.length; i++) {
-        
-        for(let j = i + 1; j < nums.length; j++) {
-            product = product * nums[j]
-        }
-        result.push((product * left))
-        left = left * nums[i]
-        product = 1
+        result.push(temp)
+        temp = temp * nums[i]
     }
+    
+    
+    temp = 1
+    for(let i = nums.length - 1; i >= 0; i--) {
+        result[i] = result[i] * temp
+        temp = temp * nums[i]
+    }
+    
     return result 
 };
